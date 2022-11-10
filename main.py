9da5f3efe6d4df50c6c8d8c8caae70c13a49ad81
IzzyLeech <ralphhunter46@gmail.com>
2022-11-10 20:52:29 +0000

    loopforchoices

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 #User choice
+
 #User input
 
 #Computer choice
 
 #Choices made as a list
+choices = ["Rock","Paper","Scissors"]
 
+def loop_for_choices():
+    
+    for i in range(len(choices)):
+        print(choices[i])
+        
+
+loop_for_choices()
 #Compare choices
