838fca644644d6f07c471c7c1723e6640a0ee6d5
Simen Daehlin <simen.dehlin@gmail.com>
2022-11-10 21:24:47 +0000

    Worked together

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ def computer_choice():
     return computer_option
 
 
-print(computer_choice())
-
 def get_answers(user_input):
     who_beats_who = {
         'rock': 'scissors',
@@ -18,23 +16,23 @@ def get_answers(user_input):
     losing_choice = who_beats_who[user_input]
     # key = user_input
     return losing_choice
-    print(losing_choice)
-#User input # Aron & Adan
 
-choices = ["rock","paper","scissors"]
+
 def take_input():
     user_input = input("type rock, paper or scissors")
-    if user_input == 'rock' || 'paper' || 'scissors':
-        return user_input
-    else :
+    if user_input == 'rock' or 'paper' or 'scissors':
+        judgement(user_input, computer_choice())
+    else:
         print('error: choice must be either rock, paper or scissors')
 
-get_answers()
 
 def judgement(user_input, computer_option):
     if user_input == computer_option:
-        print('draw') 
-    elif losing_choice == True:
+        print('draw')
+    elif losing_choice == computer_option:
         print('user wins')
     else:
         print('computer wins')
+
+
+take_input()
