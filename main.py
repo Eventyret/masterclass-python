79c820cf20da3196f4f9f515ae5d7630f1bce9b4
KeelanAnderson <keelan.anderson.ka@gmail.com>
2022-11-10 20:43:21 +0000

    choose winner

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-#User choice
-#User input
+def get_answers(key):
+    who_beats_who = {
+        'rock': 'scissors',
+        'paper': 'rock',
+        'scissors': 'paper'
+    }
+    loses = who_beats_who[key]
+    return loser
+    print(loser)
 
-#Computer choice
+get_answers('rock')
 
-#Choices made as a list
-
-#Compare choices
+if user_choice == computer_choice:
+    print('draw') 
+elif loses == computer_choice:
+    print('user wins')
+else:
+    print('computer wins')
